f12a88d48fe8d62363b16f915b87d3029daf1c02
Hamoud BLAL <118563320+hamoud31@users.noreply.github.com>
2025-11-22 07:37:00 +0100

    Update Kubernetes version and instance type

--- aws-infra-terraform-iac/variables.tf
+++ aws-infra-terraform-iac/variables.tf
@@ -37,7 +37,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version"
   type        = string
-  default     = "1.33"
+  default     = "1.31"
 }
 
 variable "node_groups" {
@@ -53,7 +53,7 @@ variable "node_groups" {
   }))
   default = {
     manager = {
-      instance_types = ["t3.micro"]
+      instance_types = ["t3.medium"]
       capacity_type  = "ON_DEMAND"
       scaling_config = {
         desired_size = 2
