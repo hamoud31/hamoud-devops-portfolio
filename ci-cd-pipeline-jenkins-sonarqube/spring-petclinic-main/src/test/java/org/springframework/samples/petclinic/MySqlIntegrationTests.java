223eace90e0f772d5c285b5a5526df48e436c462
Hamoud BLAL <118563320+hamoud31@users.noreply.github.com>
2025-11-30 02:18:16 +0100

    Upgrade MySQL container version to 9.5

--- ci-cd-pipeline-jenkins-sonarqube/spring-petclinic-main/src/test/java/org/springframework/samples/petclinic/MySqlIntegrationTests.java
+++ ci-cd-pipeline-jenkins-sonarqube/spring-petclinic-main/src/test/java/org/springframework/samples/petclinic/MySqlIntegrationTests.java
@@ -47,7 +47,7 @@ class MySqlIntegrationTests {
 
 	@ServiceConnection
 	@Container
-	static MySQLContainer container = new MySQLContainer(DockerImageName.parse("mysql:8.0"));
+	static MySQLContainer container = new MySQLContainer(DockerImageName.parse("mysql:9.5"));
 
 	@LocalServerPort
 	int port;
