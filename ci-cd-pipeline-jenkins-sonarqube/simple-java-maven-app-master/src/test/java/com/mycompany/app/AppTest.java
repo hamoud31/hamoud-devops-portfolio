4a44ffe8868b3bf6ae1ef11d77914f9ca795eca0
hamoud31 <hamoudblal@gmail.com>
2025-12-08 03:02:47 +0100

    new test update

--- ci-cd-pipeline-jenkins-sonarqube/simple-java-maven-app-master/src/test/java/com/mycompany/app/AppTest.java
+++ ci-cd-pipeline-jenkins-sonarqube/simple-java-maven-app-master/src/test/java/com/mycompany/app/AppTest.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
+
     @Test
     public void testAppMessage() {
+        // Simply create an instance of App
         App app = new App();
-        assertEquals("Hello World!", "Hello World!");
+        // Call getMessage() to cover code
+        assertEquals("Hello World!", app.getMessage());
     }
 }
 
