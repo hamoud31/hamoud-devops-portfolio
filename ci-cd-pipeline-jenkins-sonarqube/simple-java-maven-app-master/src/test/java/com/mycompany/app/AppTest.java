a17d8d0bfa28b99e023e2d31ff6fcce6e20fd3e0
hamoud31 <hamoudblal@gmail.com>
2025-12-08 03:14:33 +0100

    update test3

--- ci-cd-pipeline-jenkins-sonarqube/simple-java-maven-app-master/src/test/java/com/mycompany/app/AppTest.java
+++ ci-cd-pipeline-jenkins-sonarqube/simple-java-maven-app-master/src/test/java/com/mycompany/app/AppTest.java
@@ -6,10 +6,18 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AppTest {
 
     @Test
+    public void testAppConstructor() {
+        // Just create two instances to cover constructor
+        App app1 = new App();
+        App app2 = new App();
+        assertNotNull(app1);
+        assertNotNull(app2);
+    }
+
+    @Test
     public void testAppMessage() {
-        // Simply create an instance of App
+        // Cover getMessage() logic
         App app = new App();
-        // Call getMessage() to cover code
         assertEquals("Hello World!", app.getMessage());
     }
 }
